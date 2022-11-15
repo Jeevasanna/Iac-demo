e2823152341f45ef98f7c4aa0c39a99de937fbfd
Jeevasanna <34366273+Jeevasanna@users.noreply.github.com>
2022-11-16 01:19:24 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,10 @@
+resource "aws_instance" "myec2" {
+  count         = "${var.ec2_count}"
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  subnet_id     = "${var.subnet_id}"
 
+  tags {
+    Name = "vijayanand-Terraform"
+  }
+}
