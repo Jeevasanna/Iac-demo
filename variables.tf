c2136e08b51b50f73afb1f4c7f33534f53e89b9b
Jeevasanna <34366273+Jeevasanna@users.noreply.github.com>
2022-11-15 14:52:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "security_group" {
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "My-terraform-ec2-instance"
+  default     = "LTI-Bangalore"
 }
 
 variable "ami_id" {
