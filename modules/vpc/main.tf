0414bba01b2701141fb8c77022eff5f0c88d63d2
Jeevasanna <34366273+Jeevasanna@users.noreply.github.com>
2022-11-16 01:21:54 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,25 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "${var.vpc_cidr}"
+  instance_tenancy = "${var.tenancy}"
 
+  tags {
+    Name = "myvpc"
+  }
+}
+
+resource "aws_subnet" "mysubnet" {
+  vpc_id     = "${var.vpc_id}"
+  cidr_block = "${var.subnet_cidr}"
+
+  tags {
+    Name = "mysubnet"
+  }
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.myvpc.id}"
+}
+
+output "subnet_id" {
+  value = "${aws_subnet.mysubnet.id}"
+}
