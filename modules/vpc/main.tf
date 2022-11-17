d24e4ba968098b4a9caffbc76a8bdeba1430f3dc
Jeevasanna <34366273+Jeevasanna@users.noreply.github.com>
2022-11-17 16:13:46 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,11 +7,77 @@ resource "aws_vpc" "myvpc" {
   }
 }
 
-resource "aws_subnet" "mysubnet" {
-  vpc_id     = "${var.vpc_id}"
-  cidr_block = "${var.subnet_cidr}"
+#private subnet
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "192.168.1.0/24"
   availability_zone = "${var.az_name}"
   tags = {
-    Name = "mysubnet"
+    Name = "Private"
   }
 }
+
+#Nat subnet
+resource "aws_subnet" "nat" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "192.168.2.0/24"
+  availability_zone = "${var.az_name}"
+  tags = {
+    Name = "NAT"
+  }
+}
+
+#public subnet
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "192.168.3.0/24"
+  availability_zone = "${var.az_name}"
+  tags = {
+    Name = "Public"
+  }
+}
+
+
+# Internet Gateway
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.myvpc.id
+  tags = {
+    Name = "myigw"
+  }
+}
+
+# Route table: attach Internet Gateway
+resource "aws_route_table" "mypublic_rt" {
+  vpc_id = aws_vpc.myvpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myigw.id
+  }
+  tags = {
+    Name = "publicRouteTable"
+  }
+}
+
+# Route table: private subnet
+resource "aws_route_table" "myprivate_rt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "privateRouteTable"
+  }
+}
+
+
+
+# Route table association with private subnets
+resource "aws_route_table_association" "pvt_RT_asso" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.myprivate_rt.id
+}
+
+# Route table association with public subnets
+resource "aws_route_table_association" "pub_RT_asso" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.mypublic_rt.id
+}
+
